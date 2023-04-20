901f6171c2a84d525a17bb152a2094f1f821479b
Gustavo Leck <gustavoleck@hotmail.com>
2023-04-20 13:04:53 -0300

    fix(Add condição de parametros de busca)

--- src/routes/quantidade-musicas-by-genero-route.js
+++ src/routes/quantidade-musicas-by-genero-route.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { ConsultQuantidadeGeneroByAnoController } from "../controller/consult-quantidade-musicas-by-genero-controller.js";
 import { GenerateQuantidadeMusicasByGeneroController } from "../controller/generate-quantidade-musicas-by-genero-controller.js";
+import { ConsultQuantidadeMusicasByGeneroMiddleware } from "../middleware/consult-quantidade-musicas-by-genero-middleware.js";
 
 const router = express.Router();
 
-router.get(`/quantidade/genero`, new ConsultQuantidadeGeneroByAnoController().handle)
+router.get(`/quantidade/genero`, new ConsultQuantidadeMusicasByGeneroMiddleware().handle, new ConsultQuantidadeGeneroByAnoController().handle)
 
 router.post(`/quantidade/genero`, new GenerateQuantidadeMusicasByGeneroController().handle)
 export default router;
