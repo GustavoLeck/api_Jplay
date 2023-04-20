ea3c5c98fc1001c5880019ec97882541154e0552
Gustavo Leck <gustavoleck@hotmail.com>
2023-04-20 01:52:23 -0300

    feat(Rota de geração de atualizacao quantidade de generos)

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import status from "./routes/service-status-route.js";
 import musicas from "./routes/consult-musicas-route.js"
+import generateQuantidadeMusicaAnoGenero from "./routes/generate-quantidade-musicas-by-genero-route.js"
 
 const server = express();
 console.clear();
@@ -18,6 +19,7 @@ const port = 3050;
 server.listen(port, async () => {
     server.use("/api", status);
     server.use("/api", musicas);
+    server.use("/api", generateQuantidadeMusicaAnoGenero);
 
 
     console.log("--SERVER ON--");
