c7e1341694af667e50f3c2a728ddc10d245544e2
-v <gustavoleck@hotmail.com>
2023-04-25 23:48:23 -0300

    feat(Adicionado mensagens ao utilizar rotas)

--- src/controller/generate-lista-recomendacao-controller.js
+++ src/controller/generate-lista-recomendacao-controller.js
@@ -2,6 +2,7 @@ import { GenerateListaRecomendacao } from "../use-cases/generate-lista-recomenda
 
 export class GenerateListaRecomendacaoController {
     async handle(req, res) {
+        console.log("=> Rota de busca de lista de recomendação de musicas usada: " + req.body)
         const listaResposta = await new GenerateListaRecomendacao().execute(req.body)
         res.status(200).send(listaResposta)
     }
