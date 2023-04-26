0d24ac33488a32eebd40b50dd6f6521c45224925
Gustavo Leck <gustavoleck@hotmail.com>
2023-04-26 20:56:03 -0300

    fix(Alterado msg de rota)

--- src/controller/consult-musicas-controller.js
+++ src/controller/consult-musicas-controller.js
@@ -3,7 +3,7 @@ export class ConsultMusicasController {
     async handle(req, res) {
         const resultadoPesquisa = await new ConsultMusicas().execute(req.body)
         const totalResposta = resultadoPesquisa.length
-        console.log("=> Rota de busca de musicas usada: " + req.body)
+        console.log("=> Rota de busca de musicas usada: " + JSON.stringify(req.body))
         res.status(200).send({
             QuantidadeMusica: totalResposta,
             Musicas: resultadoPesquisa
