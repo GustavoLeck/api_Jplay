8d06f65dc9f4aa02d7d5fdf0601af5e7dd868bf4
-v <gustavoleck@hotmail.com>
2023-04-25 23:48:05 -0300

    feat(Adicionado mensagens ao utilizar rotas)

--- src/controller/consult-musicas-controller.js
+++ src/controller/consult-musicas-controller.js
@@ -3,6 +3,7 @@ export class ConsultMusicasController {
     async handle(req, res) {
         const resultadoPesquisa = await new ConsultMusicas().execute(req.body)
         const totalResposta = resultadoPesquisa.length
+        console.log("=> Rota de busca de musicas usada: " + req.body)
         res.status(200).send({
             QuantidadeMusica: totalResposta,
             Musicas: resultadoPesquisa
