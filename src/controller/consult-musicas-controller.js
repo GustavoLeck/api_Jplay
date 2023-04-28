eb4460349cd2fb805464f2ac35d068a6565919ab
Gustavo Leck <84096084+GustavoLeck@users.noreply.github.com>
2023-04-28 14:30:18 -0300

    Testesão

--- src/controller/consult-musicas-controller.js
+++ src/controller/consult-musicas-controller.js
@@ -3,6 +3,7 @@ export class ConsultMusicasController {
     async handle(req, res) {
         const resultadoPesquisa = await new ConsultMusicas().execute(req.body)
         const totalResposta = resultadoPesquisa.length
+        console.log("Teste");
         console.log("=> Rota de busca de musicas usada: " + JSON.stringify(req.body))
         res.status(200).send({
             QuantidadeMusica: totalResposta,
